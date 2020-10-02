9daee1f0385d750981e69fbc71c898391176e3b3
Plimon Howard V <plimon.howard@gmail.com>
2020-10-02 21:51:15 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -14,10 +14,6 @@ class Shoe
 
   def brand=(brand)
     @brand = brand
-    BRANDS.each do |b|
-      if (!BRANDS.include?(b))
-        BRANDS << b
-      end
-    end
+    BRANDS << brand
   end
 end
