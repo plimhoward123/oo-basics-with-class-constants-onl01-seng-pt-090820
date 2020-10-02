a69319474e137740b2791869779cba91e63fc197
Plimon Howard V <plimon.howard@gmail.com>
2020-10-02 21:45:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,11 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand, :brands
-  brands = []
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
-    brands << brand
+    BRANDS << brand
   end
 
   def cobble
