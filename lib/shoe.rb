5cb9f28bc255e788cc710a3ac915555b57247cd0
Plimon Howard V <plimon.howard@gmail.com>
2020-10-02 21:42:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     BRANDS.each do |b|
-      if (!BRANDS.include?(b))
+      if (BRANDS.include?(b))
         BRANDS << b
       end
     end
